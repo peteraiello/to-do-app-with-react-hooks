ffb85b710e69cf1d2911571b51264d98be1d8833
Peter Aiello <peter.aiello@pmwcom.co.uk>
2020-07-08 10:53:06 +0100

    Updates

---
+++ precache-manifest.6b2ca2d1a6ec83858762c6bb1d7cad7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c20a43802403bb58307cafcd177958d",
+    "url": "/ShoppingListwithReactHooks/index.html"
+  },
+  {
+    "revision": "90bf316ff668b00c63f8",
+    "url": "/ShoppingListwithReactHooks/static/css/main.1dbc51d8.chunk.css"
+  },
+  {
+    "revision": "abfdbe444daca5586d3c",
+    "url": "/ShoppingListwithReactHooks/static/js/2.22159301.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShoppingListwithReactHooks/static/js/2.22159301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90bf316ff668b00c63f8",
+    "url": "/ShoppingListwithReactHooks/static/js/main.b4ae5f54.chunk.js"
+  },
+  {
+    "revision": "845fd4e224e712639c0e",
+    "url": "/ShoppingListwithReactHooks/static/js/runtime-main.5b632693.js"
+  }
+]);
